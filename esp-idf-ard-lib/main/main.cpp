a601172dca74f811c60e333c04aaa4cdfb6fbb04
Your Name <you@example.com>
2020-04-10 12:08:28 +0530

    Added esp-idf include sample library

---
+++ esp-idf-ard-lib/main/main.cpp
@@ -0,0 +1,45 @@
+
+#include <stdio.h>
+#include <Arduino.h>
+#include <Ticker.h>
+
+extern "C" {
+void app_main(void);
+}
+
+// attach a LED to pPIO 21
+#define LED_PIN 2
+
+Ticker blinker;
+Ticker toggler;
+Ticker changer;
+float blinkerPace = 0.1;  //seconds
+const float togglePeriod = 5; //seconds
+
+void change() {
+  blinkerPace = 0.5;
+}
+
+void blink() {
+  digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+}
+
+void toggle() {
+  static bool isBlinking = false;
+  if (isBlinking) {
+    blinker.detach();
+    isBlinking = false;
+  }
+  else {
+    blinker.attach(blinkerPace, blink);
+    isBlinking = true;
+  }
+  digitalWrite(LED_PIN, LOW);  //make sure LED on on after toggling (pin LOW = led ON)
+}
+
+void app_main()
+{   
+  pinMode(LED_PIN, OUTPUT);
+  toggler.attach(togglePeriod, toggle);
+  changer.once(30, change);
+}
