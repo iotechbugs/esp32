f5f6358c988e3c867b841d7fb0e5b40b811f665d
Your Name <you@example.com>
2020-06-20 22:05:56 +0530

    Added Touch pad read example

--- esp-idf-ota/build/expat/expat/expat/lib/loadlibrary.d
+++
@@ -1,3 +0,0 @@
-expat/expat/lib/loadlibrary.o: \
- /Users/sankar/esp/esp-idf/components/expat/expat/expat/lib/loadlibrary.c
-/Users/sankar/esp/esp-idf/components/expat/expat/expat/lib/loadlibrary.c:
