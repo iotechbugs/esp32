5b2906a7834162c34b97bc6994bfdfe13daea11d
Sankar Boopathi <Sankar.Boopathi@team.telstra.com>
2020-09-28 16:07:40 +0530

    RGB LED commit

--- root/build/libsodium/libsodium/src/libsodium/sodium/version.d
+++
@@ -1,9 +0,0 @@
-libsodium/src/libsodium/sodium/version.o: \
- /Users/sankar/esp/esp-mdf/esp-idf/components/libsodium/libsodium/src/libsodium/sodium/version.c \
- /Users/sankar/esp/esp-mdf/esp-idf/components/libsodium/port_include/sodium/version.h \
- /Users/sankar/esp/esp-mdf/esp-idf/components/libsodium/libsodium/src/libsodium/include/sodium/export.h
-
-/Users/sankar/esp/esp-mdf/esp-idf/components/libsodium/port_include/sodium/version.h:
-
-/Users/sankar/esp/esp-mdf/esp-idf/components/libsodium/libsodium/src/libsodium/include/sodium/export.h:
-/Users/sankar/esp/esp-mdf/esp-idf/components/libsodium/libsodium/src/libsodium/sodium/version.c:
--- root/build/mconfig/mconfig_blufi.d
+++
@@ -1,6 +0,0 @@
-mconfig_blufi.o: \
- /Users/sankar/esp/esp-mdf/components/mconfig/mconfig_blufi.c \
- /Users/sankar/Documents/iotechbugs/code-base/github/esp32/root/build/include/sdkconfig.h
-
-/Users/sankar/Documents/iotechbugs/code-base/github/esp32/root/build/include/sdkconfig.h:
-/Users/sankar/esp/esp-mdf/components/mconfig/./mconfig_blufi.c:
